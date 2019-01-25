2327226cab10f598ffe342bb52dbb37690cfbc22
petergrr01 <peter@targetmedia.eu>
2019-01-25 09:10:50 +0100

    first copy to e-plugins

---
+++ includes/extra_datafiles/targetpay.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Digiwallet Payment Module for osCommerce
+ *
+ * @copyright Copyright 2013-2014 Yellow Melon
+ * @copyright Portions Copyright 2013 Paul Mathot
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license   see LICENSE.TXT
+ */
+define('FILENAME_TARGETPAY_TRANSACTIONS', 'targetpay_transactions.php');
+
+define('TABLE_TARGETPAY_TRANSACTIONS', 'digiwallet_transactions');
+define('TABLE_TARGETPAY_DIRECTORY', 'digiwallet_directory');
